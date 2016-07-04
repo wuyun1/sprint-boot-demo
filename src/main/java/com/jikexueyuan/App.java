f7740f514c39af0613e1c037997e3a6b2f06a0ea
wuyun1 <842269153@qq.com>
2016-07-04 13:17:10 +0800

    Initial commit on master

---
+++ src/main/java/com/jikexueyuan/App.java
@@ -0,0 +1,13 @@
+package com.jikexueyuan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
